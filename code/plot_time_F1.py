f1e3f9f70a462b4c131a92721ba25b218b40fab0
nikkiZhou <nikkizhou99@gmail.com>
2025-04-23 23:21:02 +0200

    added more results

--- code/plot_time_F1.py
+++ code/plot_time_F1.py
@@ -9,8 +9,8 @@ models = [
 file_name = 'Time_F1_Chart.png'
 
 total_time = [0.42, 1.37, 1.87, 8.28, 30, 58, 523]
-#gpu_memory = [0, 0, 0, 119, 534, 868, 9448]
-f1_score = [0.73, 0.74, 0.70, 0.61, 0.90, 0.92, 0.91]
+# gpu_memory = [0, 0, 0, 119, 534, 868, 9448]
+f1_score = [0.73, 0.74, 0.70, 0.72, 0.90, 0.92, 0.91]
 
 x = np.arange(len(models))  # X-axis positions
 
@@ -20,7 +20,7 @@ fig, axs = plt.subplots(3, 1, figsize=(12, 12))
 # Grouped Bar Chart
 width = 0.25
 axs[0].bar(x - width, total_time, width, label='Total Time (s)', color='blue')
-#axs[0].bar(x, gpu_memory, width, label='GPU Memory Used (MB)', color='green')
+# axs[0].bar(x, gpu_memory, width, label='GPU Memory Used (MB)', color='green')
 axs[0].bar(x + width, f1_score, width, label='Weighted F1-Score', color='red')
 axs[0].set_title('Grouped Bar Chart: Model Performance and Resource Usage')
 axs[0].set_xticks(x)
@@ -33,15 +33,16 @@ fig2, ax1 = plt.subplots(figsize=(12, 5))
 
 ax2 = ax1.twinx()
 ax1.plot(models, total_time, 'bo-', label='Total Time (s)')
-#ax1.plot(models, gpu_memory, 'gs-', label='GPU Memory Used (MB)')
+# ax1.plot(models, gpu_memory, 'gs-', label='GPU Memory Used (MB)')
 
 ax2.plot(models, f1_score, 'r^-', label='Weighted F1-Score')
 
 ax1.set_xlabel('Model')
-ax1.set_ylabel('Time', color='black')
+ax1.set_ylabel('Time (s)', color='black')
 ax2.set_ylabel('Weighted F1-Score', color='red')
 fig2.suptitle('Resource Usage vs F1-Score')
-ax1.tick_params(axis='x', rotation=45)
+ax1.set_xticks(x)
+ax1.set_xticklabels(models, rotation=45)
 
 # Add legends for both axes
 lines_1, labels_1 = ax1.get_legend_handles_labels()
@@ -51,10 +52,12 @@ ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper left')
 # Separate Subplots
 axs[1].bar(models, total_time, color='blue')
 axs[1].set_title('Total Time (s) per Model')
+axs[1].set_xticks(x)
 axs[1].set_xticklabels(models, rotation=45)
 
-#axs[2].bar(models, gpu_memory, color='green')
+# axs[2].bar(models, gpu_memory, color='green')
 axs[2].set_title('GPU Memory Used (MB) per Model')
+axs[2].set_xticks(x)
 axs[2].set_xticklabels(models, rotation=45)
 
 plt.tight_layout()
